3bffb7a26f82b0415724b01bbf8f673aa394eef8
Yutthakit <55013668+Yutthakit@users.noreply.github.com>
2020-12-04 08:40:33 +0700

    update

--- src/components/widget/chartLTC.js
+++ src/components/widget/chartLTC.js
@@ -1,6 +1,14 @@
-import React, { Component } from "react";
-import { Line } from "react-chartjs-2";
-import { Modal, Button, InputNumber } from "antd";
+import React, {
+  Component
+} from "react";
+import {
+  Line
+} from "react-chartjs-2";
+import {
+  Modal,
+  Button,
+  InputNumber
+} from "antd";
 import Axios from '../../config/axios.setup'
 
 export default class chartLTC extends Component {
@@ -9,34 +17,33 @@ export default class chartLTC extends Component {
     visible: false,
     name: '',
     price: 0,
-    investPerUnit: 0,
+    investPerUnit: 1,
     amount: 0,
     type: '',
     data: {
       labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
-        {
-            label: "current",
-            fill: false,
-            lineTension: 0.3,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: "butt",
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: "miter",
-            pointBorderColor: "rgba(75,192,192,0)",
-            pointBackgroundColor: "rgba(75,192,192,1)",
-            pointBorderWidth: 1.5,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,0)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 111]
-        }
-      ]}
+      datasets: [{
+        label: "current",
+        fill: false,
+        lineTension: 0.3,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,0)",
+        pointBackgroundColor: "rgba(75,192,192,1)",
+        pointBorderWidth: 1.5,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,0)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 111]
+      }]
+    }
   };
 
   componentDidMount() {
@@ -50,36 +57,70 @@ export default class chartLTC extends Component {
       console.log(err)
     });
   }
+  buyCurrency() {
+    const {
+      investPerUnit,
+      name,
+      price,
+      amount
+    } = this.state
+
+    Axios.post('/buy-currency', {
+      value_invest: amount,
+      value_per_unit: investPerUnit,
+      currency_name: name,
+      currency_price_purchase: price,
+    }).then(() => {
+      console.log('success')
+    }).catch((err) => {
+      console.log(err)
+    });
+  }
+
+  sellCurrency() {
+    const {
+      investPerUnit,
+      name,
+      price,
+    } = this.state
+    Axios.post('/sell-currency', {
+      value_per_unit: investPerUnit,
+      currency_name: name,
+      currency_price_sell: price,
+    }).then((result) => {
+      console.log(result)
+    }).catch((err) => {
+      console.log(err)
+    });
+  }
 
   getData = (data) => {
     if (data) {
       this.setState({
         amount: data.price[9],
-        data: { 
-          labels : data.date,
-          datasets:[
-            {
-              label: "current",
-              fill: false,
-              lineTension: 0.3,
-              backgroundColor: "rgba(75,192,192,0.4)",
-              borderColor: "rgba(75,192,192,1)",
-              borderCapStyle: "butt",
-              borderDash: [],
-              borderDashOffset: 0.5,
-              borderJoinStyle: "miter",
-              pointBorderColor: "rgba(75,192,192,0)",
-              pointBackgroundColor: "rgba(75,192,192,1)",
-              pointBorderWidth: 1.5,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: "rgba(75,192,192,1)",
-              pointHoverBorderColor: "rgba(220,220,220,0)",
-              pointHoverBorderWidth: 2,
-              pointRadius: 1,
-              pointHitRadius: 10,
-              data: data.price
-            }
-          ]
+        data: {
+          labels: data.date,
+          datasets: [{
+            label: "current",
+            fill: false,
+            lineTension: 0.3,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.5,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,0)",
+            pointBackgroundColor: "rgba(75,192,192,1)",
+            pointBorderWidth: 1.5,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,0)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: data.price
+          }]
         }
       })
     }
@@ -98,6 +139,15 @@ export default class chartLTC extends Component {
     this.setState({
       visible: false,
     });
+    const {
+      type
+    } = this.state
+    if (type === 'buy') {
+      this.buyCurrency()
+    } else if (type === 'sell') {
+      this.sellCurrency()
+    }
+    console.log(this.state)
   };
 
   handleCancel = () => {
@@ -114,30 +164,65 @@ export default class chartLTC extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h2>LTC</h2>
-        <Line
-          ref="chart"
-          data={this.state.data}
-        />
-        <Button type="primary" onClick={() => this.showModal('buy')}>
-            Buy
-        </Button>
-        <Button type="primary" onClick={() => this.showModal('sell')}>
-            Sell
-        </Button>
-        <Modal
-          title={this.state.name}
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <p>Current Price : { this.state.price }</p>
-          <p>Value Investment/Divestment : <InputNumber price={this.state.price} min={0.01} max={1000} defaultValue={1} onChange={this.onChange} /> </p>
-          <p>Amount : { this.state.amount  }</p>
-        </Modal>
-      </div>
+    return ( <
+      div >
+      <
+      h2 > LTC < /h2> <
+      Line ref = "chart"
+      data = {
+        this.state.data
+      }
+      /> <
+      Button type = "primary"
+      onClick = {
+        () => this.showModal('buy')
+      } >
+      Buy <
+      /Button> <
+      Button type = "primary"
+      onClick = {
+        () => this.showModal('sell')
+      } >
+      Sell <
+      /Button> <
+      Modal title = {
+        this.state.name
+      }
+      visible = {
+        this.state.visible
+      }
+      onOk = {
+        this.handleOk
+      }
+      onCancel = {
+        this.handleCancel
+      } >
+      <
+      p > Current Price: {
+        this.state.price
+      } < /p> <
+      p > Value Investment / Divestment: < InputNumber price = {
+        this.state.price
+      }
+      min = {
+        0.01
+      }
+      max = {
+        1000
+      }
+      defaultValue = {
+        1
+      }
+      onChange = {
+        this.onChange
+      }
+      /> </p >
+      <
+      p > Amount: {
+        this.state.amount
+      } < /p> <
+      /Modal> <
+      /div>
     );
   }
 
